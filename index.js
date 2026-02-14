eb1ac92d56ff7b9ae0da51f09252c3ab446e770c
idir <idirb2005@gmail.com>
2026-02-14 12:38:22 +0100

    i am tired :(

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Float nav active state
 const FloatNavsBtn = document.querySelectorAll('.float_nav a');
 const sections = document.querySelectorAll('section, header');
 const pageWrapper = document.querySelector('.page-wrapper');
@@ -16,14 +15,12 @@ FloatNavsBtn.forEach((Btn) => {
     });
 });
 
-// Scroll detection
 if (pageWrapper) {
     pageWrapper.addEventListener('scroll', () => {
         let current = '';
         
         sections.forEach(section => {
             const sectionTop = section.offsetTop;
-            const sectionHeight = section.clientHeight;
             
             if (pageWrapper.scrollTop >= (sectionTop - 200)) {
                 current = section.getAttribute('id');
@@ -41,7 +38,6 @@ if (pageWrapper) {
     });
 }
 
-// Smooth scroll for anchor links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
@@ -57,94 +53,40 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-// Resume section content
 const right = document.querySelector('.right');
 
 const expContent = `<h4>Experience</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius blanditiis est dicta, ullam error possimus dolore.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                 <ul>
-                    <li>
-                        <h6>2018 - 2019</h6>
-                        <h5>Intern</h5>
-                        <p>MTC</p>
-                    </li>
-                    <li>
-                        <h6>2019 - 2020</h6>
-                        <h5>Junior Developer</h5>
-                        <p>Tech Company</p>
-                    </li>
-                    <li>
-                        <h6>2020 - 2021</h6>
-                        <h5>Developer</h5>
-                        <p>Startup Inc</p>
-                    </li>
-                    <li>
-                        <h6>2021 - Present</h6>
-                        <h5>CS Student</h5>
-                        <p>USTHB</p>
-                    </li>
+                    <li><h6>2018 - 2019</h6><h5>Intern</h5><p>MTC</p></li>
+                    <li><h6>2019 - 2020</h6><h5>Junior Developer</h5><p>Tech Company</p></li>
+                    <li><h6>2020 - 2021</h6><h5>Developer</h5><p>Startup Inc</p></li>
+                    <li><h6>2021 - Present</h6><h5>CS Student</h5><p>USTHB</p></li>
                 </ul>`;
 
 const eduContent = `<h4>Education</h4>
-                <p>My academic journey and qualifications.</p>
+                <p>My academic journey.</p>
                 <ul>
-                    <li>
-                        <h6>2021 - Present</h6>
-                        <h5>Computer Science</h5>
-                        <p>USTHB University</p>
-                    </li>
-                    <li>
-                        <h6>2018 - 2021</h6>
-                        <h5>High School Diploma</h5>
-                        <p>Science Track</p>
-                    </li>
-                    <li>
-                        <h6>2020</h6>
-                        <h5>Web Development Course</h5>
-                        <p>Online Certification</p>
-                    </li>
+                    <li><h6>2021 - Present</h6><h5>Computer Science</h5><p>USTHB University</p></li>
+                    <li><h6>2018 - 2021</h6><h5>High School Diploma</h5><p>Science Track</p></li>
                 </ul>`;
 
 const skillsContent = `<h4>Skills</h4>
-                <p>Technologies and tools I'm proficient with.</p>
+                <p>Technologies I'm proficient with.</p>
                 <ul>
-                    <li>
-                        <h5>HTML & CSS</h5>
-                        <p>Building responsive websites</p>
-                    </li>
-                    <li>
-                        <h5>JavaScript</h5>
-                        <p>Interactive web applications</p>
-                    </li>
-                    <li>
-                        <h5>Git & GitHub</h5>
-                        <p>Version control</p>
-                    </li>
-                    <li>
-                        <h5>Problem Solving</h5>
-                        <p>Algorithms and data structures</p>
-                    </li>
+                    <li><h5>HTML & CSS</h5></li>
+                    <li><h5>JavaScript</h5></li>
+                    <li><h5>Git & GitHub</h5></li>
+                    <li><h5>Problem Solving</h5></li>
                 </ul>`;
 
 const aboutContent = `<h4>About Me</h4>
                 <p>Get to know me better!</p>
                 <ul>
-                    <li>
-                        <h5>Who Am I?</h5>
-                        <p>I'm Idir, a passionate CS student at USTHB.</p>
-                    </li>
-                    <li>
-                        <h5>What I Love</h5>
-                        <p>Coding and problem-solving.</p>
-                    </li>
-                    <li>
-                        <h5>My Goals</h5>
-                        <p>To become a skilled full-stack developer.</p>
-                    </li>
-                    <li>
-                        <h5>Hobbies</h5>
-                        <p>Gaming and exploring new frameworks.</p>
-                    </li>
+                    <li><h5>CS Student at USTHB</h5></li>
+                    <li><h5>Passionate about coding</h5></li>
+                    <li><h5>Love building projects</h5></li>
+                    <li><h5>Always learning</h5></li>
                 </ul>`;
 
 const left = document.querySelectorAll('.left button');
@@ -172,18 +114,17 @@ left.forEach((Btn) => {
     });
 });
 
-right.innerHTML = expContent;
+if (right) {
+    right.innerHTML = expContent;
+}
 
-// MixItUp
 const containerEl = document.querySelector('.projects_container');
 
-if (containerEl) {
+if (containerEl && typeof mixitup !== 'undefined') {
     let mixer = mixitup(containerEl, {
         animation: {
             enable: true,
-            duration: 400,
-            easing: 'ease',
-            effects: 'fade translateY(30px)'
+            duration: 400
         },
         selectors: {
             target: '.projects'
